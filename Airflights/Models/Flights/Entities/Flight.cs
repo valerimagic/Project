af0f86a6ebb2de51f1dbe24bc0501c21e4ccbe57
Valeri Hristov <valerimagic@gmail.com>
2021-03-19 23:50:24 +0200

    new

---
+++ Airflights/Models/Flights/Entities/Flight.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Airflights.Models.Flights.Entities
+{
+    class Flight
+    {
+    }
+}
---
+++ Airflights/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Airflights.Core.Contracts;
+using Airflights.Core.Entities;
+using Airflights.IO;
+using Airflights.IO.Contracts;
+
+namespace Airflights
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IAirFlights controller = new AirflightsController();
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+
+            Engine engine = new Engine(controller, reader, writer);
+            engine.Run();
+
+            //IPlayer player = new Player();
+        }
+    }
+}
