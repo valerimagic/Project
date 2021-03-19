6d862234ecca3b949e441016bbce252656d07ad0
Valeri Hristov <valerimagic@gmail.com>
2021-03-19 23:49:05 +0200

    new

---
+++ Airflights/IO/ConsoleWriter.cs
@@ -0,0 +1,19 @@
+﻿namespace Airflights.IO
+{
+    using System;
+
+    using Contracts;
+
+    public class ConsoleWriter : IWriter
+    {
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+    }
+}
