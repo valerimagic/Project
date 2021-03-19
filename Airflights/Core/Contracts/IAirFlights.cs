259a33d5d31c03e8d4ffe8f9c54d72f982554d83
Valeri Hristov <valerimagic@gmail.com>
2021-03-19 23:47:45 +0200

    new

---
+++ Airflights/Core/Contracts/IAirFlights.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Airflights.Core.Contracts
+{
+    public interface IAirFlights
+    {
+        string CreateAirplane();
+
+        string RemoveAirplane();
+
+        string CreateFlights();
+
+        string RemoveFlights();
+
+
+
+    }
+}
