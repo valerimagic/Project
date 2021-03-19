f2cf814765c5dac30ce0c67f1ee8c7cc50416b33
Valeri Hristov <valerimagic@gmail.com>
2021-03-19 23:57:47 +0200

    new

---
+++ Airflights/Core/Entities/Engine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Airflights.Core.Contracts;
+using Airflights.IO.Contracts;
+
+namespace Airflights.Core.Entities
+{
+    public class Engine
+    {
+
+        private readonly IAirFlights controller;
+        private readonly IReader reader;
+        private readonly IWriter writer;
+
+        public Engine(IAirFlights airflightController, IReader reader, IWriter writer)
+        {
+            this.controller = airflightController;
+            this.reader = reader;
+            this.writer = writer;
+        }
+
+        public void Run()
+        {
+
+
+
+
+        }
+
+    }
+
+}
