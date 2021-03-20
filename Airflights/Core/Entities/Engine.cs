3cd222f1e44bcb3a18ca50928a2003ae328c9ca1
Valeri Hristov <valerimagic@gmail.com>
2021-03-20 22:33:13 +0200

    update

--- Airflights/Core/Entities/Engine.cs
+++ Airflights/Core/Entities/Engine.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using Airflights.Core.Contracts;
 using Airflights.IO.Contracts;
+using Airflights.Core;
+using Airflights.Core.Entities;
 
 namespace Airflights.Core.Entities
 {
@@ -22,8 +24,59 @@ namespace Airflights.Core.Entities
 
         public void Run()
         {
+            string command = this.reader.ReadLine();
+
+            while (command != "End")
+            {
 
 
+
+                try
+                {
+
+                    string[] input = command.Split();
+                    var inputType = input[0];
+                    string resultMessage = string.Empty;
+
+
+
+
+                    if (inputType == "CreateAirplane")
+                    {
+                        resultMessage = this.controller.CreateAirplane(input[1], input[2]);
+                    }
+
+                    if (inputType == "CreateFlight")
+                    {
+                        resultMessage = this.controller.CreateFlight(input[1], input[2], input[3]);
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    this.writer.WriteLine(resultMessage);
+                }
+                catch (Exception e)
+                {
+                    this.writer.WriteLine(e.Message);
+                }
+
+
+                command = this.reader.ReadLine();
+
+            }
 
 
         }
