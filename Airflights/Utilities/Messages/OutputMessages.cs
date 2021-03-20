9709bf7a2106e053f8a4776eb5abf782f7aa1901
Valeri Hristov <valerimagic@gmail.com>
2021-03-20 22:35:12 +0200

    update

--- Airflights/Utilities/Messages/OutputMessages.cs
+++ Airflights/Utilities/Messages/OutputMessages.cs
@@ -6,5 +6,11 @@ namespace Airflights.Utilities.Messages
 {
     class OutputMessages
     {
+
+
+        public const string FlightCreated = "Flight from {0} to {1} is created!";
+
+        public const string AirPlaneCreated = "Airplane model {0} is created!";
+
     }
 }
